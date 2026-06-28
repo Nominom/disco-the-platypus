fb174a4cc7bcd923f34983f01d4ccf26decd9990
Ninja <sleepyneko@protonmail.ch>
2026-06-28 13:30:38 +0300

    Shoppa scene

---
+++ Assets/Scripts/HatScOBj.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HatScOBj", menuName = "Scriptable Objects/HatScOBj")]
+public class HatScOBj : ScriptableObject
+{
+    public GameObject hatPrefab;
+    public int hatCost;
+}
