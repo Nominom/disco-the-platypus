f3603a408a39119490e2c3581935de0d8dcae90b
Nominom <7089998+Nominom@users.noreply.github.com>
2026-06-28 13:46:30 +0300

    Fix scirp

--- Assets/Scripts/Shoppa.cs
+++ Assets/Scripts/Shoppa.cs
@@ -11,13 +11,6 @@ public class Shoppa : MonoBehaviour
 
     public GameObject Contents;
     public TextMeshProUGUI MoniesText;
-    public Button Buy1;
-    public int Buy1Price;
-    public Button Buy2;
-    public int Buy2Price;
-    public Button Buy3;
-    public int Buy3Price;
-
     public Button ExitButton;
 
     private void Awake()
@@ -31,18 +24,13 @@ public class Shoppa : MonoBehaviour
         else
         {
             Destroy(gameObject);
+            return;
         }
         
         MoniesText.text = "MONIES: " + PlayerPrefs.GetInt("Monies", 0);
-        Buy1.onClick.AddListener(BuyUno);
-        Buy2.onClick.AddListener(BuyDos);
-        Buy3.onClick.AddListener(BuyTres);
+        
         ExitButton.onClick.AddListener(ExitToMainMenu);
         
-        Buy1.GetComponentInChildren<TextMeshProUGUI>().text = "BUY " + Buy1Price;
-        Buy2.GetComponentInChildren<TextMeshProUGUI>().text = "BUY " + Buy2Price;
-        Buy3.GetComponentInChildren<TextMeshProUGUI>().text = "BUY " + Buy3Price;
-
         if (SceneManager.GetActiveScene().name == "Shoppa")
         {
             Contents.SetActive(true);
@@ -71,35 +59,6 @@ public class Shoppa : MonoBehaviour
     {
         PlayerPrefs.Save();
         SceneManager.LoadScene("MainMenuScene");
-    }
-
-    public void BuyUno()
-    {
-        if (HasMonies(Buy1Price))
-        {
-            PlayerPrefs.SetString("NotesStageMat", "Rainbow");
-            DetractMonies(Buy1Price);
-            Buy1.GetComponentInChildren<TextMeshProUGUI>().text = "SOLD";
-            Buy1.GetComponent<Button>().interactable = false;
-        }
-    }
-
-    public void BuyDos()
-    {
-        if (HasMonies(Buy2Price))
-        {
-            // TBD
-            DetractMonies(Buy2Price);
-        }
-    }
-
-    public void BuyTres()
-    {
-        if (HasMonies(Buy3Price))
-        {
-            // TBD
-            DetractMonies(Buy3Price);
-        }
     }
 
     public void DetractMonies(int price)
